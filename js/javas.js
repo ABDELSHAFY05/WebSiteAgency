6a56dd80044a8f771abc74a7f90dd76216bbc507
ABDELSHAFY05 <abdechafi.elkharta@gmail.com>
2023-01-25 00:37:43 +0100

    Last Commit

--- js/javas.js
+++ js/javas.js
@@ -5,7 +5,6 @@ var mybutton = document.getElementById("myBtn");
 window.onscroll = function () {
   scrollFunction();
 };
-
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
